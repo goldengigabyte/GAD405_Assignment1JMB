231633687139295d8ca4185ce5f793ba588b7b86
Jon Maxted <6988829+goldengigabyte@users.noreply.github.com>
2018-10-31 09:26:11 +0000

    replaced sketch.js with new

--- 00_Template/sketch.js
+++ 00_Template/sketch.js
@@ -1,25 +1,51 @@
-//DAT405 / GAD405
-//00_Template
-//The purpose of this sketch is to provide a basic template
-//for P5.js projects
-
-//Initialization function
-function setup() {
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Initialization: OK")
-  createCanvas(500,500);
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
 }
 
-//Rendering function
-function draw() {
-  r = random(255)
-  g = random(255)
-  b = random(255)
-  //Set a background color
-  background(r, g, b);
+let grid;
+let cols;
+let rows;
+let res = 4;
 
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Rendering...")
+function setup() {
+  createCanvas(400,400);
+  cols = width / res;
+  rows = height / res;
+  grid = make2DArray(cols, rows);
+}
+
+function draw() {
+  background(255);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * res;
+      let y = j * res;
+      if (grid[i][j] == 1){
+        stroke(0);
+        fill(0);
+        rect(x, y, res-1, res-1);
+      }
+    }
+  }
+  let next = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let sum = 0;
+      let state = grid[i][j];
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)){
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+
+    }
+  }
+  grid = next;
 }
